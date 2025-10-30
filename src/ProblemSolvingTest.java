538f4f1faa25e111cb84074e8af29baff150c5d1
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2025-10-30 14:14:11 -0700

    added the test for an empty set

--- src/ProblemSolvingTest.java
+++ src/ProblemSolvingTest.java
@@ -34,6 +34,15 @@ public class ProblemSolvingTest {
   //  - test a set where none start with A
   //  - test a set with only a single element
   //  - more you can think of!
+   @Test
+  void testAllStartWithA_emptyset() {
+    // arrange
+    Set<String> input =Set.of(" ");
+    // act
+    boolean actual = ProblemSolving.allStartWithA(input);
+    // assert
+    assertTrue(actual);
+  }
 
   
 
@@ -61,6 +70,7 @@ public class ProblemSolvingTest {
   // TODO:
   // Come up with more tests to thoroughly test hasEmptyString
   // Use your creativity here!
+  
 
 
   @Test
